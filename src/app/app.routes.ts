45565f26fd832065a956256a8dcf252334ed875a
Luca Albuquerque Mesquita <lucamesquitaa@gmail.com>
2024-08-06 23:24:35 -0300

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import { ContatoComponent } from './contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'cardapio', component: CardapioComponent},
+    {path: 'contato', component: ContatoComponent},
+];
